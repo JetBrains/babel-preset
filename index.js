aa49d257830ff5aa08895b1e0013d271ff7b30f1
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-12-16 22:44:05 +0300

    Spread "latest" preset into 2015,2016,2017 and add passPerPreset: true
    This fixes issue with babel-plugin-angular-annotate, because it needs to pass per preset, and needs 2015 preset to be separated
    See details https://github.com/marcioj/babel-plugin-angular-annotate#known-issues

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 module.exports = {
+  passPerPreset: true,
   presets: [
     require('babel-preset-react'),
-    require('babel-preset-latest'),
+    require('babel-preset-es2015'),
+    require('babel-preset-es2016'),
+    require('babel-preset-es2017'),
     require('babel-preset-stage-0')
   ],
   plugins: [
