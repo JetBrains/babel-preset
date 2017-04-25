17f65ec92a63f8b0a70c02c8af9495aa0057cfce
Filipp Riabchun <fryabchun@gmail.com>
2017-04-26 00:01:54 +0300

    Drop `stage-0/1`

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
     [require('babel-preset-es2015').buildPreset, {modules: false}],
     require('babel-preset-es2016'),
     require('babel-preset-es2017'),
-    require('babel-preset-stage-0')
+    require('babel-preset-stage-2')
   ],
   plugins: [
     require("babel-plugin-transform-strict-mode")
