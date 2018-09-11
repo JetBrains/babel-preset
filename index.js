0e5f8d846d78c90a0fa0738d48040d639a070906
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-09-11 14:29:41 +0300

    Return "@babel/plugin-proposal-object-rest-spread" back because it doesn't work without it
    Probably this issue https://github.com/babel/babel/issues/7215

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ module.exports = function preset(api, opts) {
       require('@babel/plugin-proposal-class-properties'),
       require('@babel/plugin-proposal-optional-chaining'),
 
+      /*
+      By unknown reason preset-env doesn't transform rest spread like `const foo = {...bar.foo, test: 123}`
+      Probably because of this issuehttps://github.com/babel/babel/issues/7215
+       */
+      require('@babel/plugin-proposal-object-rest-spread'),
+
       // Optional transformations
       angularSupport ? require('babel-plugin-angularjs-annotate') : null,
       transformRuntime ? require('@babel/plugin-transform-runtime') : null
